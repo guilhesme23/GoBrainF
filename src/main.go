703b5a01ab792f372a5f9692ccfabb5b7469b562
Guilherme Marques <marquesguilherme3@gmail.com>
2022-09-28 15:02:55 -0300

    Read code from a file

--- src/main.go
+++ src/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"bfcc/src/compiler"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Main")
-	compiler.Run("++++++++++[>++++++++>+++++++++++>++++++++++>++++>+++>++++++++>++++++++++++>+++++++++++>++++++++++>+++++++++++>+++>+<<<<<<<<<<<<-]>-.>--.>---.>++++.>++.>---.>---.>.>.>+.>+++.>.")
-	fmt.Printf("\n")
-	compiler.Run(",>++[<----->-]<[----------------------.,----------],")
+	args := os.Args[1:]
+
+	if len(args) == 0 || len(args) > 1 {
+		fmt.Println("Usage: bfcc <script_path>")
+		os.Exit(1)
+	}
+
+	path := args[0]
+	data, _ := os.ReadFile(path)
+	compiler.Run(string(data))
 }
